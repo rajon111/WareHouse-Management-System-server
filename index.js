71881302bfa69ae9c93e8046f30076f33be6576b
rajon <mostofarajon111@gmail.com>
2022-04-30 04:29:29 +0600

    in server site express-cors-nodemon-mongodb init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion,ObjectId} = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+//MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log('Example is listening at ',port)
+  })
