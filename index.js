c9af2961cdd2750e485605645b90845f166faf95
rajon <mostofarajon111@gmail.com>
2022-05-07 11:19:58 +0600

    server db connected and env added

--- index.js
+++ index.js
@@ -9,10 +9,31 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+//user-sportsWarehouse
+//pass - sbCZMRt7jFMCeuD6
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zehhr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// const collection = client.db("sportsWareHouse").collection("products");
+async function run(){
+  try{
+
+  }
+  finally{
+
+  }
+
+}
+run().catch(console.dir)
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
   
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log('Example is listening at ',port)
-  })
+})
